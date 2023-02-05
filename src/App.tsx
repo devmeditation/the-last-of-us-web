c95558ac08013ce186496e378df5feda1d0186b1
Dev Meditation <devmeditation@bcl-st.com.br>
2023-02-05 18:20:35 -0300

    feat: installing and configuring react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Home } from './pages/Home';
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Routes';
 
 // import styles from './styles/app.module.scss';
 
 export function App() {
   return (
-   <Home /> 
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter> 
   );
 }
