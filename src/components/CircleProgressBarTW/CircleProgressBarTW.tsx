0ce6f2087eead5215897503f06815730844b7adb
Dev Meditation <devmeditation@bcl-st.com.br>
2023-02-05 09:56:43 -0300

    refactor: refactoring CicleProgressBarTW component

--- src/components/CircleProgressBarTW/CircleProgressBarTW.tsx
+++ src/components/CircleProgressBarTW/CircleProgressBarTW.tsx
@@ -1,3 +1,5 @@
+import styles from "./circleProgressBarTW.module.css"
+
 interface CircleProgressBarTWProps {
   progress: string
 }
@@ -8,10 +10,7 @@ export function CircleProgressBarTW({ progress, }: CircleProgressBarTWProps) {
     <span
         style={{ background: `conic-gradient(#52965D ${progress}%, #EEEEEE 0deg)` }}
         data-progress={progress}
-        className="rotate-12 flex w-[4rem] h-[4rem] text-[0] rounded-[50%] 
-                after:-rotate-12 after:content-[attr(data-progress)'%'] after:flex after:justify-center 
-                after:flex-col after:w-[4rem] after:m-[0.25rem] after:rounded-full 
-                after:bg-background-us after:text-base after:text-center"
+        className={styles.cicleProgressBarTW}
       >
         {progress}
       </span>
